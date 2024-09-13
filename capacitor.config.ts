7f5c6034be6a6fbe2bca76b52d6dc72f4ef4515b
mahdiaslami <mahdyaslami@gmail.com>
2024-09-13 08:18:05 +0330

    build: add dev config

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,26 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'ir.rezero.bored',
   appName: 'Bored',
-  webDir: 'dist'
+  webDir: 'dist',
+  server: {
+    hostname: "localhost",
+    url: "http://localhost:5173",
+    cleartext: true,
+    allowNavigation: [
+      "http://localhost:5173"
+    ]
+  },
+  android: {
+    webContentsDebuggingEnabled: true
+  },
+  plugins: {
+    Keyboard: {
+      resizeOnFullScreen: false
+    },
+    SplashScreen: {
+      launchAutoHide: false
+    }
+  }
 };
 
 export default config;
