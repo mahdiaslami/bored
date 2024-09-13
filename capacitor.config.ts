84cb40ce4a7ab9fe346a040e1111b5a3d4cfce9b
mahdiaslami <mahdyaslami@gmail.com>
2024-09-13 07:32:05 +0330

    init vuejs and capacitorjs

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'ir.rezero.bored',
+  appName: 'Bored',
+  webDir: 'dist'
+};
+
+export default config;
